2c74a424a870a5e90a9b02477d4f5702f642bf42
zhouhaoqiang <zhouhaoqiang@163.com>
2020-03-03 21:20:30 +0800

    路由前置守卫

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/permission' // 引入限制模块
 import ElementUI from 'element-ui' // 引入element-ui
-import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-import '@/styles/index.less'
 import axios from 'axios' // 引入axios
 import Components from '@/components' // 引入组件
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/styles/index.less'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册element-ui
